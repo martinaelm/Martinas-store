ea8c8f31ffb22e2623786e67e3d52019d451d6db
martinaelm <martina@tripbirds.com>
2017-09-26 14:49:29 +0200

    uppdaterat med self

--- robot.py
+++ robot.py
@@ -3,13 +3,19 @@ class Product:
 	count = 0
 	tax = 1.25
 
+
+	def price_with_tax(self):
+		return self.price * self.count * self.tax	
+
 robot = Product()
 robot.price = 900
 robot.count = 2
+robot.tax = 1.25
 
 book = Product()
 book.price = 100
 book.count = 1
 book.tax = 1.06
 
-print(robot.price * robot.count * robot.tax + book.price * book.count * book.tax)	
+
+print(robot.price_with_tax() + book.price_with_tax())
