1f2d28daaf9b1e4b2dc5dfecc52c676d3071b02c
martinaelm <martina@tripbirds.com>
2017-09-26 15:55:02 +0200

    sista ändringarna

--- robot.py
+++ robot.py
@@ -9,9 +9,15 @@ class Product:
 		self.tax = tax
 
 	def price_with_tax(self):
-		return self.price * self.count * self.tax
+		total = self.price * self.count * self.tax
+		if total > 500:
+			return 0.9 * total
+		else:
+			return total	
 
-Robot = Product(price = 900, count = 2, tax = 1.25)
-Book = Product(price = 100, count = 1, tax = 1.06)
+products = [Product(price = 900, count = 2, tax = 1.25), Product(price = 100, count = 1, tax = 1.06)]
+total_price = 0
+for product in products:
+	total_price = total_price + product.price_with_tax()
 
-print(Robot.price_with_tax() + Book.price_with_tax())	
+print(total_price)
